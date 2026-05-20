1dd12ce141ceabe4345d3ab202cd326d5f14d6b6
urvashi29 <urvashigolu29@gmail.com>
2026-05-20 20:00:41 +0530

    first commit

---
+++ session-1/code/my-react-app/src/App.jsx
@@ -0,0 +1,21 @@
+import Header from "./Header";
+
+// Root
+// Function based component
+const App = () => {
+  // JSX
+  return (
+    <>
+      <Header />
+      <h2>Hello</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate
+        voluptatibus aliquam maxime, minima ratione ea architecto optio
+        cupiditate autem illo odio facilis quibusdam officiis deleniti nam
+        voluptates. Ab, vitae asperiores!
+      </p>
+    </>
+  );
+};
+
+export default App;
